36e1fce8f48f49042f72b0d2b0f8c5a59e4c559e
Rogerio Pereira <hi@rogeriopereira.dev>
2022-04-07 17:19:35 -0400

    Aula: 05 - Criar Página de Módulos e Aulas no Vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home/HomeView.vue'
 import MySupports from '@/views/supports/MySupports.vue'
+import ModulesAndLessons from '@/views/modules/ModulesAndLessons.vue'
 
 const routes = [
     {
@@ -11,6 +12,11 @@ const routes = [
                 path: 'minhas-duvidas',
                 name: 'campus.my.supports',
                 component: MySupports
+            },
+            {
+                path: 'modulos',
+                name: 'campus.modules',
+                component: ModulesAndLessons
             },
             {
                 path: '',
---
+++ src/views/modules/ModulesAndLessons.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="pageTitle">
+            <span class="title">Curso X</span>
+            <span class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+
+        <div class="content">
+            <div class="container">
+                <modules />
+                
+                <div class="right">
+                    <div class="content">
+                        <player />
+
+                        <supports-lesson />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Modules from './components/Modules.vue'
+import Player from './components/Player.vue'
+import SupportsLesson from './components/SupportsLesson.vue'
+
+export default {
+    components: { 
+        Modules,
+        Player,
+        SupportsLesson,
+    },
+
+    props: {
+        
+    },
+}
+</script>
