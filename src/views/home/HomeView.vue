e615b2153660ccd7b1fcad1a6a178a3b7491a98f
Rogerio Pereira <hi@rogeriopereira.dev>
2022-04-07 16:37:47 -0400

    Aula: 03 - Página de Listagem Cursos no Vue 3

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="home">
-        Listagem dos cursos
+        <div class="pageTitle">
+            <span class="title">Meus Cursos</span>
+            <span class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+
+        <div class="content">
+            <ul class="listCourses">
+                <card-course 
+                    :title="'Curso de Laravel com Vuejs'"
+                    :image="''"
+                    :bgName="'laravel'"
+                />
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import CardCourse from './components/CardCourse.vue'
 
 export default {
+    components: {
+        CardCourse
+    }
 }
 </script>
